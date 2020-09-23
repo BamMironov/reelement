83ddcaca356f032f3e6b23571a784ac9e68b9069
Artem Mironov <mironov.bam@yandex.ru>
2020-09-23 09:12:58 +0300

    refactor(reelement): Bind createHooks with component context

--- reelement.js
+++ reelement.js
@@ -3,20 +3,20 @@ import { createStore } from '@reatom/core';
 
 const store = createStore();
 
-function createHooks(component) {
-    let { _subscriptions } = component;
+function createHooks() {
+    let { _subscriptions } = this;
 
     return {
-        useAtom(atom) {
+        useAtom: (atom) => {
             if (!_subscriptions.has(atom)) {
                 _subscriptions.set(
-                    atom, store.subscribe(atom, () => component.requestUpdate())
+                    atom, store.subscribe(atom, () => this.requestUpdate())
                 )
             }
 
             return store.getState(atom);
         },
-        useAction(action) {
+        useAction: (action) => {
             return (params) => store.dispatch(action(params));
         }
     }
@@ -25,7 +25,7 @@ function createHooks(component) {
 export default function ReElement(functionalComponent, options) {
     class ComponentClass extends LitElement {
         _subscriptions = new Map();
-        _hooks = createHooks(this);
+        _hooks = createHooks.call(this);
 
         disconnectedCallback() {
             this._subscriptions.forEach(cleanup => cleanup());
